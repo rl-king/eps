a1797e0e0bb9ab98d285b9b5445f6fd0e09f8541
rl-king <hello@rubenlieking.com>
2018-09-29 21:47:39 +0200

    Save response to disk

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,22 +5,18 @@ module Lib
   ( eps
   ) where
 
+import Control.Exception
 import Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BS
 import qualified Network.HTTP.Client as Http
 import qualified Network.HTTP.Client.TLS as TLS
 
 
--- DEFINITIONS
+-- PACKAGE
 
 data Package = Package
   { packageName :: String
   , versions :: [String]
-  } deriving (Show)
-
-
-data Docs = Docs
-  { moduleName :: String
-  , comment :: String
   } deriving (Show)
 
 
@@ -30,38 +26,77 @@ instance Aeson.FromJSON Package where
       Package <$> v .: "name" <*> v .: "versions"
 
 
+instance Aeson.ToJSON Package where
+    toJSON (Package x y) = object ["name" .= x, "versions" .= y]
+    toEncoding (Package x y) = pairs ("name" .= x <> "versions" .= y)
+
+
+-- DOCS
+
+
+data Docs = Docs
+  { moduleName :: String
+  , comment :: String
+  } deriving (Show)
+
+
 instance Aeson.FromJSON Docs where
   parseJSON =
     Aeson.withObject "Docs" $ \v ->
       Docs <$> v .: "name" <*> v .: "comment"
 
 
+instance Aeson.ToJSON Docs where
+    toJSON (Docs x y) = object ["name" .= x, "comment" .= y]
+    toEncoding (Docs x y) = pairs ("name" .= x <> "comment" .= y)
+
+
 -- MAIN
 
 eps :: IO ()
 eps = do
+  packageList <- catch readCache refreshCache
+  docs <- getPackageDocs packageList
+  print packageList
+  print docs
+
+
+readCache :: IO (Maybe [Package])
+readCache = do
+  file <- BS.readFile "./cache/search.json"
+  return (Aeson.decode file :: Maybe [Package])
+
+
+refreshCache :: IOException -> IO (Maybe [Package])
+refreshCache _ = do
+  packages <- getPackageList
+  return packages
+
+
+getPackageList :: IO (Maybe [Package])
+getPackageList = do
   manager <- Http.newManager TLS.tlsManagerSettings
   request <- Http.parseRequest "https://package.elm-lang.org/search.json"
   response <- Http.httpLbs request manager
-  getPackage (Aeson.decode (Http.responseBody response) :: Maybe [Package])
+  let packages = Aeson.decode (Http.responseBody response) :: Maybe [Package]
+  BS.writeFile "./cache/search.json" (encode packages)
+  return packages
 
 
-getPackage :: Maybe ([Package]) -> IO ()
-getPackage packages =
+getPackageDocs :: Maybe ([Package]) -> IO (Maybe [Docs])
+getPackageDocs packages =
   case packages of
-    Nothing ->
-      putStrLn "meh"
-
-    Just [] ->
-      putStrLn "meh"
-
-    Just (hd:_) ->
+    Just (hd@(Package n _):_) ->
       do
         manager <- Http.newManager TLS.tlsManagerSettings
         request <- Http.parseRequest (toDocsUrl hd)
         response <- Http.httpLbs request manager
-        print (Aeson.decode (Http.responseBody response) :: Maybe [Docs])
+        let docs = Aeson.decode (Http.responseBody response) :: Maybe [Docs]
+        BS.writeFile ("./cache/foo.json") (encode docs)
+        return docs
 
+    _ ->
+      return Nothing
 
 
 toDocsUrl :: Package -> String
