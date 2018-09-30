7d7a5ce7b4256a70d1af3d727ea87ed6c18c411d
rl-king <hello@rubenlieking.com>
2018-09-30 23:45:48 +0200

    Add summary, formatting

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,8 @@ module Main ( main) where
 
 import Control.Applicative
 import Control.Exception
+import Control.Monad
+
 import Data.Aeson as Aeson
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Lazy as BS
@@ -18,20 +20,20 @@ import qualified Network.HTTP.Client.TLS as TLS
 import qualified Snap.Core as Snap
 import qualified Snap.Util.FileServe as FileServe
 import qualified Snap.Http.Server as Server
+import qualified System.IO as IO
 import Data.Ix (Ix)
 import Data.Text (Text)
 import Data.Set (Set)
 
-import Control.Monad
-import Control.Exception
-import System.IO
--- import System.Directory
-import System.Exit
+
+
+
 -- PACKAGE
 
 
 data Package = Package
   { packageName :: Text
+  , summary :: Text
   , versions :: [Text]
   } deriving (Show)
 
@@ -39,12 +41,12 @@ data Package = Package
 instance Aeson.FromJSON Package where
   parseJSON =
     Aeson.withObject "Package" $ \v ->
-      Package <$> v .: "name" <*> v .: "versions"
+      Package <$> v .: "name" <*> v .: "summary" <*> v .: "versions"
 
 
 instance Aeson.ToJSON Package where
-    toJSON (Package x y) = object ["name" .= x, "versions" .= y]
-    toEncoding (Package x y) = pairs ("name" .= x <> "versions" .= y)
+    toJSON (Package x y z) = object ["name" .= x, "summary" .= y, "versions" .= z]
+    toEncoding (Package x y z) = pairs ("name" .= x <> "summary" .= y <> "versions" .= z)
 
 
 -- DOCS
@@ -74,7 +76,7 @@ main = do
   packageList <- catch readCache refreshCache
   -- docs <- sequence $ fmap getPackageDocs (take 4 packageList)
   -- BS.writeFile ("./cache/docs.json") (encode docs)
-  -- print docs
+  print packageList
   let searchEngine = Search.insertDocs packageList initialPkgSearchEngine
   putStrLn "constructing index..."
   evaluate searchEngine >> return ()
@@ -84,15 +86,15 @@ main = do
 
   let loop = do
         putStr "search term> "
-        hFlush stdout
+        IO.hFlush IO.stdout
         t <- Text.getLine
         unless (Text.null t) $ do
           putStrLn "Ranked results:"
           let rankedResults = Search.queryExplain searchEngine (Text.words t)
 
           putStr $ unlines
-            [ show (Search.overallScore explanation) ++ ": " ++ show pkgname
-            | (explanation, pkgname) <- take 10 rankedResults ]
+            [ show (Search.termScores explanation) ++ ": " ++ show pkgname
+            | (explanation, pkgname) <- take 100 rankedResults ]
 
           loop
   return ()
@@ -132,7 +134,7 @@ getPackageDocs package = do
 
 
 toDocsUrl :: Package -> String
-toDocsUrl (Package pName pVersions) =
+toDocsUrl (Package pName _ pVersions) =
   "https://package.elm-lang.org/packages/" ++ Text.unpack pName ++ "/" ++ (Text.unpack . head) pVersions ++ "/docs.json"
 
 
@@ -173,7 +175,7 @@ searchHandler packages= do
 filterPackages :: [Package] -> Text -> Snap.Snap ()
 filterPackages packages term =
   Snap.writeLBS $ encode $
-  filter (\(Package n v) -> flip (>) 1 . length $ Text.splitOn term n) packages
+  filter (\(Package n _ _) -> flip (>) 1 . length $ Text.splitOn term n) packages
 
 
 
@@ -183,9 +185,9 @@ filterPackages packages term =
 type PkgSearchEngine =
   Search.SearchEngine Package Text PkgDocField Search.NoFeatures
 
-data PkgDocField = NameField
-                 -- | SynopsisField
-                 -- | DescriptionField
+data PkgDocField
+  = NameField
+  | SummaryField
   deriving (Eq, Ord, Enum, Bounded, Ix, Show)
 
 
@@ -196,53 +198,52 @@ initialPkgSearchEngine =
 
 pkgSearchConfig :: Search.SearchConfig Package Text PkgDocField Search.NoFeatures
 pkgSearchConfig =
-    Search.SearchConfig {
-      Search.documentKey           = (\(Package n _) -> n),
-      Search.extractDocumentTerms  = (\(Package n _) _-> Text.splitOn "/" n),
-      Search.transformQueryTerm    = normaliseQueryToken,
-      Search.documentFeatureValue  = const Search.noFeatures
+  Search.SearchConfig
+  { Search.documentKey = \(Package n _ _) -> n
+  , Search.extractDocumentTerms = extractTokens
+  , Search.transformQueryTerm = normaliseQueryToken
+  , Search.documentFeatureValue = const Search.noFeatures
   }
   where
-    -- extractTokens :: Package -> PkgDocField -> [Text]
-    -- extractTokens pkg NameField = extractPackageNameTerms (display $ packageName pkg)
-    -- extractTokens pkg SynopsisField = extractSynopsisTerms stopWords (synopsis pkg)
-    -- extractTokens pkg DescriptionField = extractDescriptionTerms stopWords (description pkg)
+    extractTokens :: Package -> PkgDocField -> [Text]
+    extractTokens (Package n _ _) NameField =
+      Text.split (\c -> c == '/' || c == '-') $ Text.toLower n
+    extractTokens (Package _ s _) SummaryField = Text.words s
 
     normaliseQueryToken :: Text -> PkgDocField -> Text
     normaliseQueryToken tok =
       let tokFold = Text.toCaseFold tok
           -- tokStem = stem English tokFold
-       in \field -> case field of
-                      NameField        -> tokFold
-                      -- SynopsisField    -> tokStem
-                      -- DescriptionField -> tokStem
+      in \field ->
+        case field of
+          NameField -> tokFold
+          SummaryField -> tokFold
+
 
 defaultSearchRankParameters :: Search.SearchRankParameters PkgDocField Search.NoFeatures
 defaultSearchRankParameters =
-    Search.SearchRankParameters {
-      Search.paramK1,
-      Search.paramB,
-      Search.paramFieldWeights,
-      Search.paramFeatureWeights = Search.noFeatures,
-      Search.paramFeatureFunctions = Search.noFeatures,
-      Search.paramResultsetSoftLimit = 200,
-      Search.paramResultsetHardLimit = 400,
-      Search.paramAutosuggestPrefilterLimit  = 500,
-      Search.paramAutosuggestPostfilterLimit = 500
-    }
+  Search.SearchRankParameters
+  { Search.paramK1 = paramK1
+  , Search.paramB = paramB
+  , Search.paramFieldWeights = paramFieldWeights
+  , Search.paramFeatureWeights = Search.noFeatures
+  , Search.paramFeatureFunctions = Search.noFeatures
+  , Search.paramResultsetSoftLimit = 200
+  , Search.paramResultsetHardLimit = 400
+  , Search.paramAutosuggestPrefilterLimit  = 500
+  , Search.paramAutosuggestPostfilterLimit = 500
+  }
   where
     paramK1 :: Float
     paramK1 = 1.5
 
     paramB :: PkgDocField -> Float
     paramB NameField = 0.9
-    -- paramB SynopsisField    = 0.5
-    -- paramB DescriptionField = 0.5
+    paramB SummaryField = 0.5
 
     paramFieldWeights :: PkgDocField -> Float
-    paramFieldWeights NameField        = 20
-    -- paramFieldWeights SynopsisField    = 5
-    -- paramFieldWeights DescriptionField = 1
+    paramFieldWeights NameField = 10
+    paramFieldWeights SummaryField = 5
 
 
 stopWords :: Set Search.Term
