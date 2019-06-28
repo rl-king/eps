c4bb0385335f8ef85f10c230898768dc12cb7229
rl-king <hello@rubenlieking.com>
2019-06-28 15:06:39 +0200

    Experiment with Free

--- src/haskell/lib/Mock.hs
+++ src/haskell/lib/Mock.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
 module Mock where
 
+import Control.Monad.Free
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as Text
 import Data.Aeson as Aeson
@@ -12,35 +14,56 @@ import qualified Network.HTTP.Client.TLS as TLS
 import Data.Package
 
 
+data FetchF next
+    = FetchModules Http.Manager Package next
+    | FetchPackages Http.Manager ([Package] -> next)
+    deriving (Functor)
+
+type Fetch a =
+  Free FetchF a
+
+run :: Fetch a -> IO a
+run = foldFree morph
+  where
+    morph :: FetchF a -> IO a
+    morph tf =
+      case tf of
+        FetchModules m package@Package{_pName, _pVersions} next -> do
+          response <- (request m . toLatestVersionDocUrl) package
+          createDirectoryIfMissing True cacheDir
+          LBS.writeFile (cacheDir ++ "/docs.json") (Http.responseBody response)
+          return next
+          where
+            cacheDir =
+              Text.unpack $ Text.intercalate "/" ["./cache", _pName, last _pVersions]
+            toLatestVersionDocUrl Package{_pName, _pVersions} =
+              Text.unpack $ Text.intercalate "/"
+              ["https://package.elm-lang.org/packages", _pName, last _pVersions, "docs.json"]
+
+        FetchPackages m next -> do
+          response <- request m "https://package.elm-lang.org/search.json"
+          LBS.writeFile "./cache/search.json" (Http.responseBody response)
+          case Aeson.eitherDecode (Http.responseBody response) :: Either String [Package] of
+            Left err -> error err
+            Right packages -> next <$> return packages
+
+
+fetchPackageList :: Http.Manager -> Fetch [Package]
+fetchPackageList m =
+  Free (FetchPackages m pure)
+
+
+fetchModules :: Http.Manager -> Package -> Fetch ()
+fetchModules m p =
+  Free (FetchModules m p (pure ()))
+
+
 cachePackages :: IO ()
 cachePackages = do
   m <- Http.newManager TLS.tlsManagerSettings
-  _ <- traverse (fetchModules m) =<< fetchPackageList m
-  return ()
-
-
-fetchPackageList :: Http.Manager -> IO [Package]
-fetchPackageList m = do
-  response <- request m "https://package.elm-lang.org/search.json"
-  LBS.writeFile "./cache/search.json" (Http.responseBody response)
-  case Aeson.eitherDecode (Http.responseBody response) :: Either String [Package] of
-    Left err -> error err
-    Right packages -> return packages
-
-
-
-fetchModules :: Http.Manager -> Package -> IO ()
-fetchModules m package@Package{_pName, _pVersions}= do
-  response <- (request m . toLatestVersionDocUrl) package
-  createDirectoryIfMissing True cacheDir
-  LBS.writeFile (cacheDir ++ "/docs.json") (Http.responseBody response)
-  where
-    cacheDir =
-      Text.unpack $ Text.intercalate "/" ["./cache", _pName, last _pVersions]
-    toLatestVersionDocUrl Package{_pName, _pVersions} =
-      Text.unpack $ Text.intercalate "/"
-      ["https://package.elm-lang.org/packages", _pName, last _pVersions, "docs.json"]
-
+  run $ do
+    _ <- traverse (fetchModules m) =<< fetchPackageList m
+    return ()
 
 
 -- REQUEST
