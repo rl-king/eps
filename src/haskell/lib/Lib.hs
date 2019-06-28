ad783f1ee8e61fe84ffcc7677d1abb10e6df7194
rl-king <hello@rubenlieking.com>
2019-06-28 14:39:46 +0200

    Apply ormulu

--- src/haskell/lib/Lib.hs
+++ src/haskell/lib/Lib.hs
@@ -1,104 +1,95 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
-module Lib (main) where
+module Lib
+  ( main
+  )
+where
 
-import Control.Monad (filterM, unless, when)
-import Control.Monad.IO.Class (liftIO)
-import Control.Concurrent.STM.TVar (TVar, newTVar, readTVar, readTVarIO, modifyTVar', writeTVar)
 import Control.Concurrent (forkIO)
+import Control.Concurrent.STM.TVar (TVar, modifyTVar', newTVar, readTVar, readTVarIO, writeTVar)
+import Control.Monad (filterM, unless, when)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (ReaderT, ask, asks, runReaderT)
 import Control.Monad.STM (atomically)
-
+import Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Map.Strict as Map
-import qualified Data.Text as Text
-import Data.Aeson as Aeson
 import Data.Map.Strict (Map)
+import Data.Package
+import qualified Data.Text as Text
 import Data.Text (Text)
-
 import qualified Network.HTTP.Client as Http
 import qualified Network.HTTP.Client.TLS as TLS
 import Network.Wai.Handler.Warp
+import Request
+import qualified Search
+import qualified Search.Result as Search
 import Servant
 import qualified System.Environment as Env
 import System.IO
 
-import qualified Search.Result as Search
-import qualified Search
-import Data.Package
+-- APP
+type AppM
+  = ReaderT State Handler
 
-
-
--- STATE
-
-
-data State =
-  State
-  { _sIndex :: TVar Search.Index
-  , _sPackages :: TVar (Map Name Package)
-  }
-
+data State
+  = State
+      { _sIndex :: TVar Search.Index
+      , _sPackages :: TVar (Map Name Package)
+      }
 
 -- MAIN
-
-
 main :: IO ()
 main = do
   port <- maybe 8080 read <$> Env.lookupEnv "PORT"
   index <- atomically $ newTVar Search.emptyIndex
   packages <- atomically $ newTVar Map.empty
   let state = State index packages
+      nt s x = runReaderT x s
   _ <- forkIO $ runIndex state
-  runServer port state
-
-
+  runSettings (settings port) . serve api $ hoistServer api (nt state) handlers
 
 -- INDEX
-
-
 runIndex :: State -> IO ()
-runIndex State{_sIndex, _sPackages} = do
+runIndex State {_sIndex, _sPackages} = do
   packages <- fetchPackages
   _ <- traverse (atomically . modifyTVar' _sIndex . Search.insert) packages
-  _ <- atomically $ writeTVar _sPackages $
-    Map.fromList (fmap (\p -> (_pName p, p)) packages)
+  _ <-
+    atomically $ writeTVar _sPackages $
+      Map.fromList (fmap (\p -> (_pName p, p)) packages)
   index <- readTVarIO _sIndex
   Search.indexStats index
   return ()
 
-
-
 -- SERVER
-
-
-type Api =
-  "search" :> QueryParam "term" Text :> Get '[JSON] [Search.Result] :<|>
-  Raw
-
+type Api
+  = "search"
+    :> QueryParam "term" Text
+      :> Get '[JSON] [Search.Result]
+        :<|> Raw
 
 api :: Proxy Api
-api = Proxy
+api =
+  Proxy
 
-
-runServer :: Int -> State -> IO ()
-runServer port state =
-  runSettings settings (serve api $ handlers state)
-  where
-    settings =
-      setPort port $
-      setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port))
+settings :: Int -> Settings
+settings port =
+  setPort port $
+    setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port))
       defaultSettings
 
+handlers :: ServerT Api AppM
+handlers =
+  handleSearch :<|>
+    serveDirectoryFileServer "./"
 
-handlers :: State -> Server Api
-handlers state =
-  handleSearch state :<|>
-  serveDirectoryFileServer "./"
-
-
-handleSearch :: State -> Maybe Text -> Handler [Search.Result]
-handleSearch State{_sIndex, _sPackages} queryParam = do
+handleSearch :: Maybe Text -> AppM [Search.Result]
+handleSearch queryParam = do
+  State {_sIndex, _sPackages} <- ask
   index <- liftIO . atomically $ readTVar _sIndex
   packages <- liftIO . atomically $ readTVar _sPackages
   case queryParam of
@@ -110,15 +101,10 @@ handleSearch State{_sIndex, _sPackages} queryParam = do
     Nothing ->
       return []
 
-
-
 -- PACKAGESLIST
-
-
 fetchPackages :: IO [Package]
 fetchPackages =
   traverse fetchModules =<< fetchPackageList
-
 
 fetchPackageList :: IO [Package]
 fetchPackageList = do
@@ -127,8 +113,7 @@ fetchPackageList = do
   response <- request m "http://localhost:8080/cache/search.json"
   case Aeson.eitherDecode (Http.responseBody response) :: Either String [Package] of
     Left err -> error err
-    Right packages -> return $ take 250 packages
-
+    Right packages -> return packages
 
 fetchModules :: Package -> IO Package
 fetchModules package = do
@@ -140,15 +125,14 @@ fetchModules package = do
     Right modules ->
       return $ package {_pModules = Map.fromList $ fmap (\ms -> (_mName ms, ms)) modules}
   where
-    toLatestVersionDocUrl Package{_pName, _pVersions} =
-      Text.unpack $ Text.intercalate "/"
-      ["http://localhost:8080/cache", _pName, last _pVersions, "docs.json"]
-      -- ["https://package.elm-lang.org/packages", _pName, last _pVersions, "docs.json"]
+    toLatestVersionDocUrl Package {_pName, _pVersions} =
+      Text.unpack $
+        Text.intercalate "/"
+          ["http://localhost:8080/cache", _pName, last _pVersions, "docs.json"]
 
+-- ["https://package.elm-lang.org/packages", _pName, last _pVersions, "docs.json"]
 
 -- REQUEST
-
-
 request :: Http.Manager -> String -> IO (Http.Response LBS.ByteString)
 request m path = do
   r <- Http.parseRequest path
--- src/haskell/lib/Search.hs
+++ src/haskell/lib/Search.hs
@@ -1,59 +1,58 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
-module Search (emptyIndex, insert, indexStats, perform, Index) where
+module Search
+  ( emptyIndex
+  , insert
+  , indexStats
+  , perform
+  , Index
+  )
+where
 
 import Data.Foldable
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
-import qualified Data.Ord as Ord
-import qualified Data.Text as Text
 import Data.Map.Strict (Map)
-import Data.Text (Text)
-
-import qualified Token.Docs as Docs
-import qualified Token.TypeSig as TypeSig
-import qualified Token.Name as Name
+import qualified Data.Ord as Ord
 import Data.Package (Package)
+import qualified Data.Text as Text
+import Data.Text (Text)
 import qualified Search.Result as Result
 import Search.Result (Result)
-
-
+import qualified Token.Docs as Docs
+import qualified Token.Name as Name
+import qualified Token.TypeSig as TypeSig
 
 -- INDEX
-
-
-data Index =
-  Index
-  { _iTypeSignatures :: TypeSig.Tokens
-  , _iDefNames :: Name.Tokens
-  , _iModuleNames :: Name.Tokens
-  , _iPackageNames :: Name.Tokens
-  , _iSummaries :: Docs.Tokens
-  , _iComments :: Docs.Tokens
-  }
-
+data Index
+  = Index
+      { _iTypeSignatures :: TypeSig.Tokens
+      , _iDefNames :: Name.Tokens
+      , _iModuleNames :: Name.Tokens
+      , _iPackageNames :: Name.Tokens
+      , _iSummaries :: Docs.Tokens
+      , _iComments :: Docs.Tokens
+      }
 
 insert :: Package -> Index -> Index
 insert package (Index ts vn mn pn d c) =
   Index
-  (TypeSig.tokenize package ts)
-  (Name.tokenizeValueNames package vn)
-  (Name.tokenizeModuleNames package mn)
-  (Name.tokenizePackageNames package pn)
-  (Docs.tokenizeSummaries package d)
-  (Docs.tokenizeComments package c)
-
+    (TypeSig.tokenize package ts)
+    (Name.tokenizeValueNames package vn)
+    (Name.tokenizeModuleNames package mn)
+    (Name.tokenizePackageNames package pn)
+    (Docs.tokenizeSummaries package d)
+    (Docs.tokenizeComments package c)
 
 emptyIndex :: Index
 emptyIndex =
   Index
-  TypeSig.empty
-  Name.empty
-  Name.empty
-  Name.empty
-  Docs.empty
-  Docs.empty
-
+    TypeSig.empty
+    Name.empty
+    Name.empty
+    Name.empty
+    Docs.empty
+    Docs.empty
 
 indexStats :: Index -> IO ()
 indexStats (Index ts vn mn pn d c) =
@@ -66,83 +65,74 @@ indexStats (Index ts vn mn pn d c) =
     , show (Docs.size c) ++ " : indexed comments"
     ]
 
-
 -- SEARCHING
-
-
 perform :: Text -> Map Text Package -> Index -> ([String], [Result])
 perform term packages index =
-  let
-    packageNames = Name.query term 10 (_iPackageNames index)
-    moduleNames = Name.query term 8 (_iModuleNames index)
-    defNames = Name.query term 6 (_iDefNames index)
-    summaries = Docs.query term (_iSummaries index)
-    comments = Docs.query term (_iComments index)
-    typeSigs = TypeSig.query term (_iTypeSignatures index)
-    info =
-      [ show (Map.size packageNames) ++ " : package names"
-      , show (Map.size moduleNames) ++ " : module names"
-      , show (Map.size defNames) ++ " : def names"
-      , show (length summaries) ++ " : summaries"
-      , show (length comments) ++ " : comments"
-      , show (length typeSigs) ++ " : type sigs"
-      ]
-  in
-    case toScope term of
-      TypeSigs ->
-        ( "TYPESIGS":info
-        , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd)
-          $ Map.toList packageNames ++ Map.toList moduleNames
-          ++ rank packageNames moduleNames (Map.toList defNames)
-          ++ rank packageNames moduleNames typeSigs
-        )
-      ModulesAndPackages ->
-        ( "MODULES AND PACKAGES":info
-        , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd)
-          $ Map.toList packageNames ++ Map.toList moduleNames
-          ++ rank packageNames moduleNames (Map.toList defNames)
-        )
-      Rest ->
-        ( "REST":info
-        , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd)
-          $ Map.toList packageNames ++ Map.toList moduleNames
-          ++ Map.toList comments ++ Map.toList summaries
-          ++ rank packageNames moduleNames (Map.toList defNames)
-        )
+  let packageNames = Name.query term 10 (_iPackageNames index)
+      moduleNames = Name.query term 8 (_iModuleNames index)
+      defNames = Name.query term 6 (_iDefNames index)
+      summaries = Docs.query term (_iSummaries index)
+      comments = Docs.query term (_iComments index)
+      typeSigs = TypeSig.query term (_iTypeSignatures index)
+      info =
+        [ show (Map.size packageNames) ++ " : package names"
+        , show (Map.size moduleNames) ++ " : module names"
+        , show (Map.size defNames) ++ " : def names"
+        , show (length summaries) ++ " : summaries"
+        , show (length comments) ++ " : comments"
+        , show (length typeSigs) ++ " : type sigs"
+        ]
+  in case toScope term of
+       TypeSigs ->
+         ( "TYPESIGS" : info
+         , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd) $
+           Map.toList packageNames ++
+           Map.toList moduleNames ++
+           rank packageNames moduleNames (Map.toList defNames) ++
+           rank packageNames moduleNames typeSigs
+         )
+       ModulesAndPackages ->
+         ( "MODULES AND PACKAGES" : info
+         , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd) $
+           Map.toList packageNames ++
+           Map.toList moduleNames ++
+           rank packageNames moduleNames (Map.toList defNames)
+         )
+       Rest ->
+         ( "REST" : info
+         , Result.toSearchResults packages . take 10 . List.sortOn (Ord.Down . snd) $
+           Map.toList packageNames ++
+           Map.toList moduleNames ++
+           Map.toList comments ++
+           Map.toList summaries ++
+           rank packageNames moduleNames (Map.toList defNames)
+         )
 
-
-rank ::
-  Map Result.Info Int ->
-  Map Result.Info Int ->
-  [(Result.Info, Int)] ->
-  [(Result.Info, Int)]
+rank
+  :: Map Result.Info Int
+  -> Map Result.Info Int
+  -> [(Result.Info, Int)]
+  -> [(Result.Info, Int)]
 rank packages modules typeSigs =
   promote <$> typeSigs
   where
-    promote (info, points) =
-      case info of
-        Result.ValueRef pName mName _ ->
-          let
-            pMember = Map.member (Result.PackageRef pName) packages
+    promote (info, points) = case info of
+      Result.ValueRef pName mName _ ->
+        let pMember = Map.member (Result.PackageRef pName) packages
             mMember = Map.member (Result.ModuleRef pName mName) modules
-          in
-            case (pMember, mMember) of
-              (True, True) -> (info, points + 15)
-              (False, True) -> (info, points + 10)
-              (True, False) -> (info, points + 5)
-              (False, False) -> (info, points)
-        _ ->
-          (info, points)
-
+        in case (pMember, mMember) of
+             (True, True) -> (info, points + 15)
+             (False, True) -> (info, points + 10)
+             (True, False) -> (info, points + 5)
+             (False, False) -> (info, points)
+      _ ->
+        (info, points)
 
 -- SCOPE
-
-
 data Scope
   = TypeSigs
   | ModulesAndPackages
   | Rest
-
 
 toScope :: Text -> Scope
 toScope query
